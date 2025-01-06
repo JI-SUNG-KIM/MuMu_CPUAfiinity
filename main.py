9b12fccfbbf8e48354e1df5097fd0691cac13a13
JI-SUNG-KIM <43037863+JI-SUNG-KIM@users.noreply.github.com>
2025-01-06 22:33:00 +0900

    1.1
    Signed-off-by: JI-SUNG-KIM <43037863+JI-SUNG-KIM@users.noreply.github.com>

--- main.py
+++ main.py
@@ -18,12 +18,14 @@ def count_process(name):
 
     return int(returned.stdout)
 
-def set_affinity(name, count):
+def set_affinity(name, count, ccd):
+    #             2^16-1, 2^32-2^16, 2^32-1
+    ccd_to_num = [65535, 4294901760, 4294967295]
     command = f"""
     $process = Get-Process {name}
     """
     for i in range(count):
-        command += f"\n$process[{i}].ProcessorAffinity = 65535"
+        command += f"\n$process[{i}].ProcessorAffinity = {ccd_to_num[ccd]}"
 
     subprocess.run(
         ["powershell", "-Command", command],
@@ -33,12 +35,17 @@ def set_affinity(name, count):
 
 
 process_name_list = ["MuMuPlayer", "MuMuVMMHeadless"]
+ccd_chosen = -1
+
 for process_name in process_name_list:
     print(f"Working for {process_name}")
     process_count = count_process(process_name)
+
     if process_count == 0:
         print("Error!")
     elif process_count > 0:
-        set_affinity(process_name, process_count)
-        print(f"Setting Completed for {process_count} Instance(s).")
+        ccd_chosen = int(input("0 for CCD0, 1 for CCD1, 2 for all : ")) if ccd_chosen == -1 else ccd_chosen
+        set_affinity(process_name, process_count, ccd_chosen)
+        print(f"Setting to ccd{ccd_chosen} Completed for {process_count} Instance(s).")
+
 input("")

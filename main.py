bf470a3c7d0183d7df4d97e8863d05711c42eb3e
JI-SUNG-KIM <43037863+JI-SUNG-KIM@users.noreply.github.com>
2025-01-08 22:54:04 +0900

    1.1.2
    Signed-off-by: JI-SUNG-KIM <43037863+JI-SUNG-KIM@users.noreply.github.com>

--- main.py
+++ main.py
@@ -80,6 +80,13 @@ for process_name in process_name_list:
         set_affinity(process_name, process_count, ccd_chosen)
         print(f"Setting to ccd{ccd_chosen} Completed for {process_count} Instance(s).")
 
+'''
+thread에서 돌고 있는 input이 종료되지 않아서 다음 input이 있으면 thread input이 먼저 입력받고 그다음 여기 input이 입력됨.
+multiprocessing은 생성한 프로세스에서 input 못 받음.
+키보드 입력 구현은 포커스가 다른 창으로 넘어가면 문제 발생
+
+'''
+
 a = input("\nhi?: ")
 print(f"at the end : {a}")
 
